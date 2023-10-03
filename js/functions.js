3a3b851a3f2ab36b1860d7f4b69a799b8b1797cb
takustova <kustova.tatiana@gmail.com>
2023-10-03 20:58:09 +0200

    functions_commit

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,14 @@
 const checkLineLength = function(string, maxLength) {
   return (string.length <= maxLength);
 };
-console.log(checkLineLength('проверяемая строка', 20));
-console.log(checkLineLength('проверяемая строка', 18));
-console.log(checkLineLength('проверяемая строка', 10));
+checkLineLength('проверяемая строка', 20);
 const checkIfPalindrom = function (string) {
   const newString = string.replaceAll(' ', '').toLowerCase();
   let emptyString = '';
   for (let i = string.length - 1; i >= 0; i--) {
     emptyString += newString[i];
-}
+  }
   return (newString === emptyString);
 };
-console.log(checkIfPalindrom('топот'));
-console.log(checkIfPalindrom('ДовОд'));
-console.log(checkIfPalindrom('Кекс'));
+checkIfPalindrom('топот');
+
