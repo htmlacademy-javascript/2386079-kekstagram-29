364a6c759a5ccd1c9e4c78a9ae77d13a9501458e
Keks <keks@htmlacademy.ru>
2024-03-17 20:57:07 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,25 @@
+import {renderGallery} from './thumbnails.js';
+import {setOnFormSubmit, closeImgUploadForm} from './form.js';
+import {getData, sendData} from './api.js';
+import {showAlert} from './util.js';
+import {showSuccessMessage, showErrorMessage} from './message.js';
+import {setEffectSlider} from './effects.js';
+
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    closeImgUploadForm();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
+
+try {
+  const data = await getData();
+  renderGallery(data);
+} catch(err) {
+  showAlert(err.message);
+}
+
+setEffectSlider();
